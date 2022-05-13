8d2413708de932b17eeae21c1aaabf6c3be3590b
Ali Faraji <ali.faraji90@gmail.com>
2022-05-14 01:13:38 +0200

    Create basic alert and its story.

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { Button, ButtonProps, Dialog, DialogActions, DialogContent, DialogContentText, DialogProps, DialogTitle } from '@mui/material'
+import React, { ReactNode } from 'react'
 
-function Alert() {
+interface AlertProps extends DialogProps {
+    title?: string,
+    message: ReactNode | ReactNode[],
+    actions: (Omit<ButtonProps, "children"> & { label: string })[]
+}
+
+function Alert({ title, message, actions = [{ label: "OK" }], ...dialogProps }: AlertProps) {
     return (
-        <div>Alert</div>
+        <Dialog {...dialogProps}>
+            {title && <DialogTitle>{title}</DialogTitle>}
+            <DialogContent>
+                {typeof message === "string" ?
+                    <DialogContentText>{message}</DialogContentText>
+                    :
+                    message
+                }
+            </DialogContent>
+            <DialogActions>
+                {actions.map(
+                    ({ label, ...action }, index) => (
+                        <Button {...action} key={index}>
+                            {label}
+                        </Button>
+                    )
+                )}
+            </DialogActions>
+        </Dialog>
     )
 }
 
